0d52a4742f8c9d6f01b3a3da87a3618f61c3ab37
Slok-e <106457601+Slok-e@users.noreply.github.com>
2022-06-30 01:54:53 -0400

    Update File_manage.py

--- File_manage.py
+++ File_manage.py
@@ -1,11 +1,8 @@
-import os
-import sys
 import time
 import shutil
 import logging
-import fileinput
-from os import listdir
-from fileinput import filename
+from os import scandir, rename
+from os.path import splitext, exists, join
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -14,44 +11,64 @@ dest_image = "/Users/kelsobroderick/Desktop/Download\(Sorted\)/Images"
 dest_music = "/Users/kelsobroderick/Desktop/Download\(Sorted\)/Music"
 dest_sfx = "/Users/kelsobroderick/Desktop/Download\(Sorted\)/Music/Sfx"
 dest_video = "/Users/kelsobroderick/Desktop/Download\(Sorted\)/Video"
+dest_documents = "/Users/kelsobroderick/Desktop/Download\(Sorted\)/Documents"
+
+# Image file types:
+image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw",
+                    ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
+# Video file types:
+video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
+                    ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
+# Audio file types:
+audio_extensions = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
+# Document file types:
+document_extensions = [".doc", ".docx", ".odt",
+                       ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
 
 # Function to add 1 to repeated file name
 def makeUnique(dest, name):
-    filename, extension = os.path.splitext(path)
+    filename, extension = splitext(name)
     counter = 1
-    while os.path.exists(path):
-        path = filename + "(" + str(counter) +")" + extension
+    while exists(f"{dest}/{name}"):
+        path = (f"{filename}({str(counter)}){extension}")
         counter += 1
     
-
     return path
 
 #Function to move a file
 def move(dest, entry, name):
-    file_exist = os.path.exists(dest + "/" + name)
-    if file_exist:
-        unique_name = makeUnique(name)
-        os.rename(entry,unique_name)
-    shutil.move(entry,dest)
+    if exists(f"{dest}/{name}"):
+        unique_name = makeUnique(dest,name)
+        oldName = join(dest,name)
+        newName = join(dest,unique_name)
+        rename(entry,unique_name)
+    move(entry,dest)
 
+# This runs when
 class Handler(FileSystemEventHandler):
     def on_modified(self, event):
-       with os.scandir(source_dir) as entries:
+       with scandir(source_dir) as entries:
         for entry in entries:
             name = entry.name
-            dest = source_dir
-            if name.endswith(".wav") or name.endswith(".mp3") or name.endwitg(".mp4"):
-                if fileinput
-            return super().on_modified(event)
-    
+        for audio_extensions in audio_extensions:
+            if name.endswith(audio_extensions) or name.endswith(audio_extensions.upper()):
+                if entry.stat().st_size < 10000000 or "SFX" in name:
+                    dest = dest_sfx
+                else:
+                    dest = dest_music
+                move(dest, entry, name)
+                logging.info(f"Moved audio file: (name)")
+        for video_extensions in video_extensions:
+            if name.endswith(video_extensions) or name.endswith(video_extensions.upper()):
+                move(video_extensions, entry, name)
+                logging.info(f"Moved video file: {name}")
 
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
+    if __name__ == "__main__":
+        logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
     path = source_dir
-    event_handler = Handler()
+    event_handler = FileSystemEventHandler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
